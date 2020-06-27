32a055f1ee8be59bf1ac5dea25a53328d47ae6c6
Md-Junaeid-Alam <md.junaeid,alam.se@gmail.com>
2020-06-27 18:07:52 +0600

    First commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (movie) => {
+    console.log("Like Clicked", movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: movieCount } = this.state.movies;
+    if (movieCount == 0) return <p>There is no movie in database</p>;
+    return (
+      <React.Fragment>
+        <p>Showing {movieCount} in following table</p>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <th>{movie.title}</th>
+                <th>{movie.genre.name}</th>
+                <th>{movie.numberInStock}</th>
+                <th>{movie.dailyRentalRate}</th>
+                <th>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </th>
+                <th>
+                  <button>
+                    <div
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </div>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+export default Movies;
